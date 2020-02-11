c9f4ea09fe5de2d72ad381c1b6e5ec0adb5fbd62
RuslanKussein <ruslankusein@gmail.com>
2020-02-11 13:53:02 +0600

    adding features

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function (){
+    //Fixed Scroll Down Bug issues
+    /*
+    window.onscroll = function() {myFunction()};
+    var header = document.getElementById("header-nav");
+    var sticky = header.offsetTop;
+    function myFunction() {
+        if (window.pageYOffset > sticky) {
+            header.classList.add("sticky");
+        } else {
+            header.classList.remove("sticky");
+        }
+    }
+    */
+
+});
