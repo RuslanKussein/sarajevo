18c89e5a40bc74e0cbd847292e78ddbe392ec473
RuslanKussein <ruslankusein@gmail.com>
2020-02-12 02:16:30 +0600

    facts+discuss

--- scripts/main.js
+++ scripts/main.js
@@ -56,34 +56,4 @@ $(document).ready(function (){
             $('button.button[val]').show(300);
         }
     });
-
-    //DEMO
-    var slideIndex = 1;
-    showSlides(slideIndex);
-
-// Next/previous controls
-    function plusSlides(n) {
-        showSlides(slideIndex += n);
-    }
-
-// Thumbnail image controls
-    function currentSlide(n) {
-        showSlides(slideIndex = n);
-    }
-
-    function showSlides(n) {
-        var i;
-        var slides = document.getElementsByClassName("mySlides");
-        var dots = document.getElementsByClassName("dot");
-        if (n > slides.length) {slideIndex = 1}
-        if (n < 1) {slideIndex = slides.length}
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-        }
-        for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(" active", "");
-        }
-        slides[slideIndex-1].style.display = "block";
-        dots[slideIndex-1].className += " active";
-    }
 });
