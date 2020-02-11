a5d62b0dbb254787f9848e8d2c97e6f7643fd633
RuslanKussein <ruslankusein@gmail.com>
2020-02-11 23:05:31 +0600

    works

--- scripts/main.js
+++ scripts/main.js
@@ -13,4 +13,77 @@ $(document).ready(function (){
     }
     */
 
+    //Navbar scroll
+    $('ul.menu a[href^="#"]').click(function () {
+        var target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 1500);
+        $('ul.menu a[href^="#"]').css({
+            'color': '#fff'
+        });
+        $(this).css({
+            'color': '#4c76ff'
+        });
+        return false;
+    });
+    //Navbar color
+    $(window).scroll(function () {
+        if ($(this).scrollTop() != 0) {
+            $('ul.menu a[href^="#"]').css({
+                'color': '#fff'
+            });
+            $('ul.menu a[href="#Home"]').css({
+                'color': '#4c76ff'
+            });
+        }
+    });
+    //Portfolio
+    $('button.button[val]').click(function () {
+        if ($(this).attr('val') != 'all' && $(this).attr('bool') == 'off') {
+            $('button.button[bool]').attr('bool', 'off');
+            $(this).attr('bool', 'on');
+            $('button.button[val]').removeClass('focused');
+            $(this).addClass('focused');
+            //Suda mozhno dobavit(tipa novie image i zapushit)
+            var nameOfValue=$(this).attr('val');
+            $('button.button[val='+nameOfValue+']').show(300);
+        } else if ($(this).attr('val') == 'all' && $(this).attr('bool') == 'off') {
+            $('button[bool]').attr('bool', 'off');
+            $(this).attr('bool', 'on');
+            $('button[bool]').removeClass('focused');
+            $(this).addClass('focused');
+            $('button.button[val]').show(300);
+        }
+    });
+
+    //DEMO
+    var slideIndex = 1;
+    showSlides(slideIndex);
+
+// Next/previous controls
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+// Thumbnail image controls
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("dot");
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex-1].style.display = "block";
+        dots[slideIndex-1].className += " active";
+    }
 });
